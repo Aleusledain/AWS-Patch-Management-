529519d0947eb488d16a8356ad8307e79cbb8048
McIntyre <150527506+Aleusledain@users.noreply.github.com>
2025-10-14 21:07:19 -0400

    variables.tf

---
+++ environments/dev/variables.tf
@@ -0,0 +1,5 @@
+variable "vpc_security_group_ids" { type = list(string) }
+variable "subnet_id"              { type = string }
+variable "ami"                    { type = string }
+variable "instance_type"          { type = string }
+variable "instance_name"          { type = string }
