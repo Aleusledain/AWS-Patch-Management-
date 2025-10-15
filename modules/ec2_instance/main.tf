c5386ca94746234f71c589201ac214710b58b41d
McIntyre <150527506+Aleusledain@users.noreply.github.com>
2025-10-14 21:03:55 -0400

    main.tf

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -1,10 +1,11 @@
-resource "aws_instance" "linux" {
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2
-  instance_type = "t2.micro"
-  iam_instance_profile = var.instance_profile
-  tags = {
-    Name        = "PatchableLinux"
-    PatchGroup  = "DevGroup"
-    Environment = "Dev"
-  }
+module "ec2_instance" {
+  source                 = "../../modules/ec2_instance"
+  instance_profile       = module.iam_role_ssm.ssm_profile.name
+  vpc_security_group_ids = var.vpc_security_group_ids
+  subnet_id              = var.subnet_id
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  name                   = var.instance_name
+  aws_ssm_patch_baseline = module.patch_baseline_linux.baseline.id
+  aws_iam_role           = module.iam_role_ssm.ssm_role.name
 }
